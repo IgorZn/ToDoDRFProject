7b4675b21aa6ab8db5c14c8fcb07b8d3cf29575c
IgorZn <igor.znamensky@gmail.com>
2023-08-29 10:06:35 +0300

    next: done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,37 @@
 import './App.css';
 import React from 'react';
+import axios from "axios";
 
-const list = [
-    {
-        "id": 1,
-        "title": "1st todo",
-        "body": "Learn Django properly."
-    },
-    {
-        "id": 2,
-        "title": "Second item",
-        "body": "Learn Python."
-    },
-    {
-        "id": 3,
-        "title": "Learn HTTP",
-        "body": "It's important."
-    }
-]
+const HOST = 'http://127.0.0.1:8000/api/'
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {list};
+    state = {
+            todos: []
+        };
+
+    componentDidMount() {
+        this.getTodos();
     }
+
+    getTodos() {
+        axios
+            .get(HOST)
+            .then(res => {
+                this.setState({ todos: res.data })
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
 
     render() {
         return (
             <div>
-                {this.state.list.map(item => (
+                {this.state.todos.map(item => (
                     <div key={item.id}>
                         <h1>{item.title}</h1>
-                        <p>{item.body}</p>
+                        <span>{item.body}</span>
                     </div>
                 ))}
             </div>
